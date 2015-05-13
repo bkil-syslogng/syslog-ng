f6e520a0108174125ba1bd65a36904b7f1990f99
Laszlo Budai <Laszlo.Budai@balabit.com>
2015-05-13 14:40:33 +0200

    configure_opts: disable systemd
    Signed-off-by: Laszlo Budai <Laszlo.Budai@balabit.com>

--- sbuild/configure_opts.py
+++ sbuild/configure_opts.py
@@ -1,1 +1,1 @@
-default_opts="--enable-debug --with-ivykis=system --with-libesmtp=\${prefix} --disable-amqp --with-embedded-crypto"
+default_opts="--enable-debug --with-ivykis=system --with-libesmtp=\${prefix} --disable-amqp --with-embedded-crypto --disable-systemd"
