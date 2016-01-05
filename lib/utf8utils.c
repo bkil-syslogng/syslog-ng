d4d5e79654de6e36d3ab94b64126a1de29bf64e7
bkil-syslogng <tamas.nagy@balabit.com>
2016-01-05 15:53:50 +0100

    FIXUP lib/utf8utils.c: deduplicated to _append_unsafe_utf8_as_escaped()
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- lib/utf8utils.c
+++ lib/utf8utils.c
@@ -32,25 +32,24 @@
  * "text" format, we never embed anything that would become not valid utf8
  * when decoded.
  *
- * This is basically meant to be used when sending data to
- * 8 bit clean receivers, e.g. syslog-ng or WELF.
- *
  * Here are the rules that the routine follows:
  *   - well-known control characters are escaped (0x0a as \n and so on)
- *   - other control characters as \xXX
+ *   - other control characters as per control_format
  *   - backslash is escaped as \\
  *   - any additional characters (only ASCII is supported) as \<char>
- *   - invalid utf8 sequences are converted to \xXX
+ *   - invalid utf8 sequences are converted as per invalid_format
  *   - utf8 characters are reproduced as is
  */
-void
-append_unsafe_utf8_as_escaped_binary (GString *escaped_string, const gchar *str,
-                                      gssize str_len, const gchar *unsafe_chars)
+static void
+_append_unsafe_utf8_as_escaped (GString *escaped_output, const gchar *raw,
+                                      gssize raw_len, const gchar *unsafe_chars,
+                                      const gchar *control_format,
+                                      const gchar *invalid_format)
 {
-  if (str_len < 0)
-    str_len = strlen(str);
-  const gchar *char_ptr = str;
-  const gchar *const end = char_ptr + str_len;
+  if (raw_len < 0)
+    raw_len = strlen(raw);
+  const gchar *char_ptr = raw;
+  const gchar *const end = char_ptr + raw_len;
 
   while (char_ptr < end)
     {
@@ -60,41 +59,53 @@ append_unsafe_utf8_as_escaped_binary (GString *escaped_string, const gchar *str,
         {
           case (gunichar) -1:
           case (gunichar) -2:
-            g_string_append_printf(escaped_string, "\\x%02x", *(guint8 *) char_ptr);
+            g_string_append_printf(escaped_output, invalid_format, *(guint8 *) char_ptr);
             char_ptr++;
             continue;
             break;
           case '\b':
-            g_string_append(escaped_string, "\\b");
+            g_string_append(escaped_output, "\\b");
             break;
           case '\f':
-            g_string_append(escaped_string, "\\f");
+            g_string_append(escaped_output, "\\f");
             break;
           case '\n':
-            g_string_append(escaped_string, "\\n");
+            g_string_append(escaped_output, "\\n");
             break;
           case '\r':
-            g_string_append(escaped_string, "\\r");
+            g_string_append(escaped_output, "\\r");
             break;
           case '\t':
-            g_string_append(escaped_string, "\\t");
+            g_string_append(escaped_output, "\\t");
             break;
           case '\\':
-            g_string_append(escaped_string, "\\\\");
+            g_string_append(escaped_output, "\\\\");
             break;
           default:
             if (uchar < 32)
-              g_string_append_printf(escaped_string, "\\x%02x", uchar);
+              g_string_append_printf(escaped_output, control_format, uchar);
             else if (uchar < 256 && unsafe_chars && strchr(unsafe_chars, (gchar) uchar))
-              g_string_append_printf(escaped_string, "\\%c", (gchar) uchar);
+              g_string_append_printf(escaped_output, "\\%c", (gchar) uchar);
             else
-              g_string_append_unichar(escaped_string, uchar);
+              g_string_append_unichar(escaped_output, uchar);
             break;
         }
       char_ptr = g_utf8_next_char(char_ptr);
     }
 }
 
+/**
+ * This is basically meant to be used when sending data to
+ * 8 bit clean receivers, e.g. syslog-ng or WELF.
+ * @see _append_unsafe_utf8_as_escaped()
+ */
+void
+append_unsafe_utf8_as_escaped_binary (GString *escaped_string, const gchar *str,
+                                      gssize str_len, const gchar *unsafe_chars)
+{
+  _append_unsafe_utf8_as_escaped (escaped_string, str, str_len, unsafe_chars,
+                                  "\\x%02x", "\\x%02x");
+}
 gchar *
 convert_unsafe_utf8_to_escaped_binary (const gchar *str, gssize str_len,
                                        const gchar *unsafe_chars)
@@ -108,75 +119,16 @@ convert_unsafe_utf8_to_escaped_binary (const gchar *str, gssize str_len,
 }
 
 /**
- * This function escapes an unsanitized input (e.g. that can contain binary
- * characters, and produces an escaped format that can be deescaped in need,
- * which is guaranteed to be utf8 clean.  The major difference between
- * "binary" and "text" form is that the receiver is able to cope with \xXX
- * sequences that can incorporate invalid utf8 sequences when decoded.  With
- * "text" format, we never embed anything that would become not valid utf8
- * when decoded.
- *
  * This is basically meant to be used when sending data to
  * utf8 only receivers, e.g. JSON.
- *
- * Here are the rules that the routine follows:
- *   - well-known control characters are escaped (0x0a as \n and so on)
- *   - other control characters as \u00XX
- *   - backslash is escaped as \\
- *   - any additional characters (only ASCII is supported) as \<char>
- *   - invalid utf8 sequences are converted to \\xXX (e.g. double-escaped as a literal \xXX sequence)
- *   - utf8 characters are reproduced as is
- **/
+ * @see _append_unsafe_utf8_as_escaped()
+ */
 void
 append_unsafe_utf8_as_escaped_text (GString *escaped_string, const gchar *str,
                                     gssize str_len, const gchar *unsafe_chars)
 {
-  if (str_len < 0)
-    str_len = strlen(str);
-  const gchar *char_ptr = str;
-  const gchar *const end = char_ptr + str_len;
-
-  while (char_ptr < end)
-    {
-      gunichar uchar = g_utf8_get_char_validated(char_ptr, end - char_ptr);
-
-      switch (uchar)
-        {
-          case (gunichar) -1:
-          case (gunichar) -2:
-            g_string_append_printf(escaped_string, "\\\\x%02x", *(guint8 *) char_ptr);
-            char_ptr++;
-            continue;
-            break;
-          case '\b':
-            g_string_append(escaped_string, "\\b");
-            break;
-          case '\f':
-            g_string_append(escaped_string, "\\f");
-            break;
-          case '\n':
-            g_string_append(escaped_string, "\\n");
-            break;
-          case '\r':
-            g_string_append(escaped_string, "\\r");
-            break;
-          case '\t':
-            g_string_append(escaped_string, "\\t");
-            break;
-          case '\\':
-            g_string_append(escaped_string, "\\\\");
-            break;
-          default:
-            if (uchar < 32)
-              g_string_append_printf(escaped_string, "\\u%04x", uchar);
-            else if (uchar < 256 && unsafe_chars && strchr(unsafe_chars, (gchar) uchar))
-              g_string_append_printf(escaped_string, "\\%c", (gchar) uchar);
-            else
-              g_string_append_unichar(escaped_string, uchar);
-            break;
-        }
-      char_ptr = g_utf8_next_char(char_ptr);
-    }
+  _append_unsafe_utf8_as_escaped (escaped_string, str, str_len, unsafe_chars,
+                                  "\\u%04x", "\\\\x%02x");
 }
 
 gchar *
