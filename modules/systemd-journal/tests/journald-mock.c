9c742a0ffa9b16facf4d9e5fff645989a1d68851
bkil-syslogng <tamas.nagy@balabit.com>
2016-02-16 12:22:40 +0100

    unit tests: some warning fixes
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/systemd-journal/tests/journald-mock.c
+++ modules/systemd-journal/tests/journald-mock.c
@@ -181,7 +181,12 @@ journald_mock_new()
 {
   Journald *self = g_new0(Journald, 1);
 
-  pipe(self->fds);
+  int result = pipe(self->fds);
+  if (result < 0)
+    {
+      g_free(self);
+      return NULL;
+    }
   g_fd_set_nonblock(self->fds[0], TRUE);
   g_fd_set_nonblock(self->fds[1], TRUE);
 
