b69b0e3ae17604aea9099b6adaa6946480c2da5a
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-05 11:23:54 +0100

    lib, libtest: some warning fixes (full prototypes, const gchar*)
    INVARIANT: BINARY
    
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- lib/apphook.h
+++ lib/apphook.h
@@ -40,12 +40,12 @@ enum
 typedef void (*ApplicationHookFunc)(gint type, gpointer user_data);
 
 void register_application_hook(gint type, ApplicationHookFunc func, gpointer user_data);
-void app_startup();
-void app_post_daemonized();
-void app_pre_config_loaded();
-void app_post_config_loaded();
+void app_startup(void);
+void app_post_daemonized(void);
+void app_pre_config_loaded(void);
+void app_post_config_loaded(void);
 void app_thread_start(void);
 void app_thread_stop(void);
-void app_shutdown();
+void app_shutdown(void);
 
 #endif
