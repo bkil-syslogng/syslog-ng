b2813126db7fd19e954495b78f9416839868a8fa
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-07 10:52:25 +0100

    modules/afmongodb, configure.ac: ENABLE_LEGACY_MONGODB_OPTIONS
    This re-adds additional support for previous syntax used by libmongo-client
    before we started using mongo-c-driver and its URI syntax exclusively.
    
    I.e., so additionally to uri() and collection(), now the following
    keywords should work again:
    
    servers()
    safe_mode()
    path()
    password()
    username()
    database()
    
    Note that these are plainly translated to a connection URI without
    much sanity checking or preserving their former semantic meaning.
    So various aspects of the MongoDB connection like health checks, retries,
    error reporting and synchronicity will still follow the slightly altered
    semantics of mongo-c-driver.
    
    In the future, theoretically it is enough to revert this single commit
    to remove support.
    (This is one reason I did not opt for OOP & inheritance to implement this)
    
    Fixes #893
    
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/afmongodb/afmongodb-legacy-uri.h
+++ modules/afmongodb/afmongodb-legacy-uri.h
@@ -20,53 +20,13 @@
  *
  */
 
-#ifndef AFMONGODB_PRIVATE_H_
-#define AFMONGODB_PRIVATE_H_
+#ifndef AFMONGODB_LEGACY_URI_H_
+#define AFMONGODB_LEGACY_URI_H_
 
-#include "mongoc.h"
-#include "logthrdestdrv.h"
-#include "string-list.h"
-#include "value-pairs/value-pairs.h"
+#include "afmongodb-private.h"
 
-typedef struct _MongoDBDestDriver
-{
-  LogThrDestDriver super;
-
-  /* Shared between main/writer; only read by the writer, never
-   written */
-  gchar *coll;
-  GString *uri_str;
-
-#if SYSLOG_NG_ENABLE_LEGACY_MONGODB_OPTIONS
-  GList *servers;
-  gchar *address;
-  gint port;
-
-  gboolean safe_mode;
-  gchar *user;
-  gchar *password;
-#endif
-
-  LogTemplateOptions template_options;
-
-  time_t last_msg_stamp;
-
-  ValuePairs *vp;
-
-  /* Writer-only stuff */
-#if SYSLOG_NG_ENABLE_LEGACY_MONGODB_OPTIONS
-  GList *recovery_cache;
-  gboolean is_legacy;
-  gchar *db;
-#endif
-
-  const gchar *const_db;
-  mongoc_uri_t *uri_obj;
-  mongoc_client_t *client;
-  mongoc_collection_t *coll_obj;
-
-  GString *current_value;
-  bson_t *bson;
-} MongoDBDestDriver;
+void afmongodb_dd_init_legacy(MongoDBDestDriver *self);
+void afmongodb_dd_free_legacy(MongoDBDestDriver *self);
+gboolean afmongodb_dd_create_uri_from_legacy(MongoDBDestDriver *self);
 
 #endif
