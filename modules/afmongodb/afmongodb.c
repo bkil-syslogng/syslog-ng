6032059635ec99e805470776ee40b29305ecaa25
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-07 10:52:25 +0100

    modules/afmongodb, configure.ac: ENABLE_LEGACY_MONGODB_OPTIONS
    This re-adds additional support for previous syntax used by libmongo-client
    before we started using mongo-c-driver and its URI syntax exclusively.
    
    I.e., so additionally to uri() and collection(), now the following
    keywords should work again:
    
    servers()
    safe_mode()
    path()
    password()
    username()
    database()
    
    Note that these are plainly translated to a connection URI without
    much sanity checking or preserving their former semantic meaning.
    So various aspects of the MongoDB connection like health checks, retries,
    error reporting and synchronicity will still follow the slightly altered
    semantics of mongo-c-driver.
    
    A large part of the code comes from the previous state before being
    removed:
    
    66368985f04f3c64e853cf0be6f5640542af4429
    "afmongodb: added mongo uri option, removed all redundant ones"
    
    In the future, theoretically it is enough to revert this single commit
    to remove support.
    (This is one reason I did not opt for OOP & inheritance to implement this)
    
    Fixes #893
    
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>
    (cherry picked from commit b5cd1e3f6bbb0d52b9f543370835d22bd7c4ea27)

--- modules/afmongodb/afmongodb.c
+++ modules/afmongodb/afmongodb.c
@@ -35,6 +35,11 @@
 
 #include <time.h>
 
+#include "afmongodb-private.h"
+#if SYSLOG_NG_ENABLE_LEGACY_MONGODB_OPTIONS
+#include "afmongodb-legacy-uri.h"
+#endif
+
 #define DEFAULT_URI \
       "mongodb://127.0.0.1:27017/syslog"\
       "?wtimeoutMS=60000&socketTimeoutMS=60000&connectTimeoutMS=60000"
@@ -434,6 +439,11 @@ afmongodb_dd_private_uri_init(LogDriver *d)
 {
   MongoDBDestDriver *self = (MongoDBDestDriver *)d;
 
+#if SYSLOG_NG_ENABLE_LEGACY_MONGODB_OPTIONS
+  if (!afmongodb_dd_create_uri_from_legacy(self))
+    return FALSE;
+#endif
+
   if (!self->uri_str)
     self->uri_str = g_string_new(DEFAULT_URI);
 
@@ -538,6 +548,9 @@ _logpipe_free(LogPipe *d)
       self->uri_str = NULL;
     }
   g_free(self->coll);
+#if SYSLOG_NG_ENABLE_LEGACY_MONGODB_OPTIONS
+  afmongodb_dd_free_legacy(self);
+#endif
   if (self->vp)
     value_pairs_unref(self->vp);
 
@@ -583,6 +596,9 @@ afmongodb_dd_new(GlobalConfig *cfg)
   self->super.stats_source = SCS_MONGODB;
   self->super.messages.retry_over = _worker_retry_over_message;
 
+#if SYSLOG_NG_ENABLE_LEGACY_MONGODB_OPTIONS
+  afmongodb_dd_init_legacy(self);
+#endif
   afmongodb_dd_set_collection(&self->super.super.super, "messages");
 
   log_template_options_defaults(&self->template_options);
