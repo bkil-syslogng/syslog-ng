545410d010c057a2eddf5855ccfa8f0ad485bca7
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-07 09:36:22 +0100

    modules/afmongodb/afmongodb.c: only destroy connector if non-null
    Otherwise, mongoc throws SIGSEGV.
    
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/afmongodb/afmongodb.c
+++ modules/afmongodb/afmongodb.c
@@ -415,6 +415,7 @@ _worker_thread_deinit(LogThrDestDriver *d)
   MongoDBDestDriver *self = (MongoDBDestDriver *)d;
 
   g_string_free(self->current_value, TRUE);
+  self->current_value = NULL;
 
   bson_destroy(self->bson);
   self->bson = NULL;
@@ -465,8 +466,10 @@ _logpipe_free(LogPipe *d)
   g_free(self->coll);
   value_pairs_unref(self->vp);
 
-  mongoc_uri_destroy(self->uri_obj);
-  mongoc_collection_destroy(self->coll_obj);
+  if (self->uri_obj)
+    mongoc_uri_destroy(self->uri_obj);
+  if (self->coll_obj)
+    mongoc_collection_destroy(self->coll_obj);
   mongoc_cleanup();
   log_threaded_dest_driver_free(d);
 }
