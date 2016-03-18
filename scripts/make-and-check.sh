6e4aa64f47161f367fa4509123d5f0bbc62fbbd4
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-18 19:13:05 +0100

    .travis.yml: add verification of CFLAGS propagation from build log
    Fixes #903
    
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

---
+++ scripts/make-and-check.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+. `dirname "$0"`/../tests/build-log-cflags-propagation.sh
+
+main() {
+  local LOG="make.log"
+  echo "info: log saved to $LOG" >&2
+  {
+    logged_main "$@"
+    echo "exit status: $?"
+  } 2>&1 |
+  tee "$LOG"
+}
+
+logged_main() {
+  exec_prop_check "make -j V=1" ||
+  S=$?;
+  if [ "$S" = "42" -o "$S" = "0" ]; then
+    return $S;
+  else
+    make V=1 --keep-going # to make error messages more readable on error
+    return 1
+  fi
+
+  export CFLAGS="$CFLAGS -Werror"
+  exec_prop_check "make -j 1 distcheck V=1 --keep-going" &&
+
+  make func-test V=1 --keep-going
+}
+
+main "$@"
+
---
+++ tests/build-log-cflags-propagation.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+exec_prop_check() {
+  local ERROREXIT="42"
+  local CMD="$1"
+  local BUILDLOG="build.$$.log"
+  {
+    eval "$CMD"
+    echo $?
+  } |
+  tee "$BUILDLOG" |
+  grep --line-buffered --invert-match --extended-regexp "^(\
+libtool: (link|relink|install): |\
+depbase=|\
+(test -z|rm|\./lib/merge-grammar.pl|\./doc/mallard2man\.py) |\
+`printf "\t"`?/bin/bash |\
+`printf "\t"`?(gcc|mv) \
+)"
+
+  S=`tail -n 1 "$BUILDLOG"`
+  if [ "$S" -eq 0 ]; then
+    build_log_cflags_propagation "$BUILDLOG"
+    S=$?
+    rm "$BUILDLOG"
+    return $S
+  else
+    rm "$BUILDLOG"
+    return $ERROREXIT
+  fi
+}
+
+####
+# private functions:
+
+build_log_cflags_propagation() {
+  local BUILDLOG="$1"
+  local NOTPROP="`find_not_prop "$BUILDLOG"`"
+  if [ -z "$NOTPROP" ]; then
+    echo "info: CFLAGS propagation test passed" >&2
+    true
+  else
+    local N=`echo "$NOTPROP" | wc -l`
+    echo "error: -Wshadow/-Werror did not propagate via CFLAGS in $N cases:" >&2
+
+    printf "%s\n" "$NOTPROP" |
+    sed "s~^~> ~" >&2
+    false
+  fi
+}
+
+find_not_prop() {
+  {
+    find_gcc "$@" |
+    ignore_submodule_gcc |
+    grep -vE -- " -Wshadow( |.* )-Werror "
+  } 2>&1
+}
+
+find_gcc() {
+  grep -E "^(\
+libtool: compile: +gcc |\
+`printf "\t"`?/bin/bash \./libtool  --tag=CC   --mode=compile gcc |\
+`printf "\t"`?gcc \
+)" "$@"
+}
+
+ignore_submodule_gcc() {
+# mongo-c-driver, 2*rabbitmq-c and 7*ivykis respectively:
+  grep -vE -- "\<(\
+gcc( -std=gnu99)? -DPACKAGE_NAME=\\\\\"(libbson|mongo-c-driver)\\\\\"|\
+\
+gcc -std=gnu99 -DHAVE_CONFIG_H -I\. +-I\./librabbitmq |\
+gcc -std=gnu99 -DHAVE_CONFIG_H -I\. +-I\.\./\.\./\.\./\.\./modules/afamqp/rabbitmq-c |\
+\
+gcc -DHAVE_CONFIG_H -I\. -I\.\./\.\. +-D_GNU_SOURCE -I\.\./\.\./src/include -I\.\./\.\./src/include |\
+gcc -DHAVE_CONFIG_H -I\. -I\.\./\.\.  -I\.\./\.\./src/include -I\.\./\.\./src/include |\
+gcc -DHAVE_CONFIG_H -I\. -I\.\. +-D_GNU_SOURCE -I\.\./src/include -I\.\./src/include |\
+gcc( -std=gnu99)? -DHAVE_CONFIG_H -I\. -I\.\./\.\./\.\./\.\./lib/ivykis/src |\
+gcc( -std=gnu99)? -DHAVE_CONFIG_H -I\. -I\.\./\.\./\.\./\.\./lib/ivykis/test |\
+gcc( -std=gnu99)? -DHAVE_CONFIG_H -I\. -I\.\./\.\./\.\./\.\./\.\./lib/ivykis/contrib/iv_getaddrinfo |\
+gcc -std=gnu99 -DHAVE_CONFIG_H -I\. -I\.\./\.\./\.\./\.\./\.\./lib/ivykis/contrib/kojines \
+)" "$@"
+}
+
