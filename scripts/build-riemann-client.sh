3d1a3b8a8a2e69a1d09418ec79a690e397c4c387
bkil-syslogng <tamas.nagy@balabit.com>
2016-05-06 18:44:04 +0200

    f scripts/build-riemann protobuf unneeded

--- scripts/build-riemann-client.sh
+++ scripts/build-riemann-client.sh
@@ -1,45 +1,6 @@
 #!/bin/sh -x
 
 build_riemann_client() {
-#  build_protobufc || exit 1
-  build_riemann_client_core || exit 1
-}
-
-build_protobufc() {
-  build_protobuf
-  build_protobufc_core
-}
-
-build_protobuf() {
-  if ! pkg-config --exists "protobuf >= 2.6.0"; then
-    git clone "https://github.com/google/protobuf" || exit 1
-    cd protobuf || exit 1
-    ./autogen.sh || exit 1
-    ./configure --prefix="$SLNG_CACHE" \
-      --disable-maintainer-mode \
-      --disable-dependency-tracking || exit 1
-    make -j 4 install || exit 1
-    cd .. || exit 1
-    rm -Rf protobuf
-  fi
-  true
-}
-
-build_protobufc_core() {
-  if ! which protoc-c; then
-    git clone "https://github.com/protobuf-c/protobuf-c" || exit 1
-    cd protobuf-c || exit 1
-    ./autogen.sh || exit 1
-    ./configure --prefix="$SLNG_CACHE" \
-      --disable-dependency-tracking || exit 1
-    make -j 4 install || exit 1
-    cd .. || exit 1
-    rm -Rf protobuf-c
-  fi
-  true
-}
-
-build_riemann_client_core() {
   if ! pkg-config --exists "riemann-client >= 1.6.0"; then
     git clone https://github.com/algernon/riemann-c-client.git || exit 1
     cd riemann-c-client || exit 1
